d2051dc441702cdee269398ee98292b991403627
Jonathan M. Henson <henso@amazon.com>
2019-01-15 14:21:19 -0800

    Removed stupid cmake cache stuff.

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/local/ANT/henso/Source/aws-iot-device-sdk-cpp-v2
-# Build directory: /home/local/ANT/henso/Source/aws-iot-device-sdk-cpp-v2/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
