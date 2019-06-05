6b280d20c5c9c997d8ebde153095886bab01bca0
Michael Graeb <graebm@amazon.com>
2019-06-05 09:11:32 -0700

    Build with shared libs in CI

--- codebuild/common-windows.bat
+++ codebuild/common-windows.bat
@@ -9,13 +9,13 @@ mkdir %INSTALL_DIR%
 mkdir %BUILDS_DIR%\aws-crt-cpp-build
 cd %BUILDS_DIR%\aws-crt-cpp-build
 git clone --branch v0.4.0 https://github.com/awslabs/aws-crt-cpp.git
-cmake %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DCMAKE_PREFIX_PATH="%INSTALL_DIR%" -DCMAKE_BUILD_TYPE="Release" -DBUILD_DEPS=ON aws-crt-cpp || goto error
+cmake %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DCMAKE_PREFIX_PATH="%INSTALL_DIR%" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE="Release" -DBUILD_DEPS=ON aws-crt-cpp || goto error
 cmake --build . --target install || goto error
 
 @rem build SDK
 mkdir %BUILDS_DIR%\aws-iot-device-sdk-cpp-v2-build
 cd %BUILDS_DIR%\aws-iot-device-sdk-cpp-v2-build
-cmake %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DCMAKE_PREFIX_PATH="%INSTALL_DIR%" -DCMAKE_BUILD_TYPE="Release" %CODEBUILD_SRC_DIR% || goto error
+cmake %CMAKE_ARGS% -DCMAKE_INSTALL_PREFIX="%INSTALL_DIR%" -DCMAKE_PREFIX_PATH="%INSTALL_DIR%" -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE="Release" %CODEBUILD_SRC_DIR% || goto error
 cmake --build . || goto error
 
 goto :EOF
