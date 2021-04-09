5dc3aa9ab568ade96f27b899878e05f1c9f893e2
Michael Graeb <graebm@amazon.com>
2021-04-09 22:19:48 +0000

    Don't build samples in byo-crypto CI
    moved sample build steps to python script so they could be skipped based on logic

---
+++ .builder/actions/build_samples.py
@@ -0,0 +1,32 @@
+import Builder
+import os
+import sys
+
+
+class BuildSamples(Builder.Action):
+    def run(self, env):
+        if env.args.cli_config['variables'].get('skip_samples', "0") != "0":
+            print('skip_samples is defined. Skipping samples...')
+            return
+
+        steps = []
+        samples = [
+            'samples/mqtt/basic_pub_sub',
+            'samples/mqtt/raw_pub_sub',
+            'samples/shadow/shadow_sync',
+            'samples/greengrass/basic_discovery',
+            'samples/identity/fleet_provisioning',
+            'samples/jobs/describe_job_execution',
+        ]
+        for sample_path in samples:
+            build_path = os.path.join('build', sample_path)
+            steps.append(['cmake',
+                          f'-B{build_path}',
+                          f'-H{sample_path}',
+                          f'-DCMAKE_PREFIX_PATH={env.install_dir}',
+                          '-DCMAKE_BUILD_TYPE=RelWithDebInfo'])
+            steps.append(['cmake3',
+                          '--build', build_path,
+                          '--config', 'RelWithDebInfo'])
+
+        return Builder.Script(steps)
