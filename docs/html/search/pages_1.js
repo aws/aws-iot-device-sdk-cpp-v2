0224a20b4fb124bf3a6938fdb744b1c164bcfec2
Joseph Klix <jkl@amazon.com>
2021-08-23 10:52:36 -0700

    move doxygen output to docs folder for github pages

---
+++ docs/html/search/pages_1.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['code_20of_20conduct_0',['Code of Conduct',['../md__c_o_d_e__o_f__c_o_n_d_u_c_t.html',1,'']]],
+  ['contributing_20guidelines_1',['Contributing Guidelines',['../md__c_o_n_t_r_i_b_u_t_i_n_g.html',1,'']]]
+];
---
+++ docs/html/search/pages_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['frequently_20asked_20questions_0',['Frequently Asked Questions',['../md__f_a_q.html',1,'']]]
+];
