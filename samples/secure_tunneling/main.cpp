82ebc7e6f0f1484e65653b28f09ff5477b3f9fc9
Steve Kim <sbstevek@amazon.com>
2021-10-20 11:38:22 -0700

    Added sleep to prevent maximum cpu usage in sample

--- samples/secure_tunneling/main.cpp
+++ samples/secure_tunneling/main.cpp
@@ -15,9 +15,11 @@
 #include <iostream>
 
 #include <algorithm>
+#include <chrono>
 #include <condition_variable>
 #include <iostream>
 #include <mutex>
+#include <thread>
 
 using namespace std;
 using namespace Aws::Iot;
@@ -170,7 +172,8 @@ int main(int argc, char *argv[])
     /*
      * This will execute when an mqtt connect has completed or failed.
      */
-    auto onConnectionCompleted = [&](Mqtt::MqttConnection &, int errorCode, Mqtt::ReturnCode returnCode, bool) {
+    auto onConnectionCompleted = [&](Mqtt::MqttConnection &, int errorCode, Mqtt::ReturnCode returnCode, bool)
+    {
         if (errorCode)
         {
             fprintf(stdout, "Connection failed with error %s\n", ErrorDebugString(errorCode));
@@ -186,7 +189,8 @@ int main(int argc, char *argv[])
     /*
      * Invoked when a disconnect message has completed.
      */
-    auto onDisconnect = [&](Mqtt::MqttConnection & /*conn*/) {
+    auto onDisconnect = [&](Mqtt::MqttConnection & /*conn*/)
+    {
         {
             fprintf(stdout, "Disconnect completed\n");
             connectionClosedPromise.set_value();
@@ -207,7 +211,8 @@ int main(int argc, char *argv[])
     }
 
     auto onSubscribeToTunnelsNotifyResponse = [&](Aws::Iotsecuretunneling::SecureTunnelingNotifyResponse *response,
-                                                  int ioErr) -> void {
+                                                  int ioErr) -> void
+    {
         if (ioErr == 0)
         {
             fprintf(stdout, "Received MQTT Tunnel Notification\n");
@@ -250,7 +255,8 @@ int main(int argc, char *argv[])
         }
     };
 
-    auto OnSubscribeComplete = [&](int ioErr) -> void {
+    auto OnSubscribeComplete = [&](int ioErr) -> void
+    {
         if (ioErr)
         {
             fprintf(stderr, "MQTT Connection failed with error %d\n", ioErr);
@@ -273,6 +279,7 @@ int main(int argc, char *argv[])
     }
     while (1)
     {
+        std::this_thread::sleep_for(500ms);
         continue;
     }
 }
