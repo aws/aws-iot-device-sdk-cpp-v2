7fac42840839016a8e1ddc1087ac444dca578b00
Steve Kim <sbstevek@amazon.com>
2021-11-15 13:41:30 -0800

    Formatting

--- samples/mqtt/basic_pub_sub/main.cpp
+++ samples/mqtt/basic_pub_sub/main.cpp
@@ -459,7 +459,8 @@ int main(int argc, char *argv[])
     };
 
     auto onInterrupted = [&](Mqtt::MqttConnection &, int error) { 
-        fprintf(stdout, "Connection interrupted with error %s\n", ErrorDebugString(error)); };
+        fprintf(stdout, "Connection interrupted with error %s\n", ErrorDebugString(error));
+        };
 
     auto onResumed = [&](Mqtt::MqttConnection &, Mqtt::ReturnCode, bool) { fprintf(stdout, "Connection resumed\n"); };
 
@@ -523,25 +524,25 @@ int main(int argc, char *argv[])
         std::promise<void> subscribeFinishedPromise;
         auto onSubAck =
             [&](Mqtt::MqttConnection &, uint16_t packetId, const String &topic, Mqtt::QOS QoS, int errorCode) {
-            if (errorCode)
-            {
-                fprintf(stderr, "Subscribe failed with error %s\n", aws_error_debug_str(errorCode));
-                exit(-1);
-            }
-            else
-            {
-                if (!packetId || QoS == AWS_MQTT_QOS_FAILURE)
+                if (errorCode)
                 {
-                    fprintf(stderr, "Subscribe rejected by the broker.");
+                    fprintf(stderr, "Subscribe failed with error %s\n", aws_error_debug_str(errorCode));
                     exit(-1);
                 }
+                else
+                {
+                    if (!packetId || QoS == AWS_MQTT_QOS_FAILURE)
+                    {
+                        fprintf(stderr, "Subscribe rejected by the broker.");
+                        exit(-1);
+                    }
                 else
                 {
                     fprintf(stdout, "Subscribe on topic %s on packetId %d Succeeded\n", topic.c_str(), packetId);
                 }
             }
-            subscribeFinishedPromise.set_value();
-        };
+                subscribeFinishedPromise.set_value();
+            };
 
         connection->Subscribe(topic.c_str(), AWS_MQTT_QOS_AT_LEAST_ONCE, onMessage, onSubAck);
         subscribeFinishedPromise.get_future().wait();
