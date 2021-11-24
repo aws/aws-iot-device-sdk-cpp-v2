1ee491ffc2022d9f69ace41f7d6d6766f429f9c0
Steve Kim <sbstevek@amazon.com>
2021-11-23 16:24:40 -0800

    More Testing

--- codebuild/samples/securetunnel-linux.sh
+++ codebuild/samples/securetunnel-linux.sh
@@ -1,25 +1,30 @@
 #!/bin/bash
-echo "bin/bash"
+
 set -e
-echo "-e set"
+
 env
-echo "env"
+
 pushd $CODEBUILD_SRC_DIR/samples/secure_tunneling/secure_tunnel
-echo "pushd"
+
 mkdir _build
 cd _build
 cmake -DCMAKE_PREFIX_PATH=/tmp/install ..
 make -j
-echo "make-j"
+
+echo "make -j"
+
 REGION=$(aws configure get region)
-echo "REGION set"
+
+echo "got region"
+
 read -r a[{1..10}] <<< $(aws iotsecuretunneling open-tunnel)
-echo "TUNNEL CREATED"
 SOURCETOKEN=${a[7]}
 DESTINATIONTOKEN=${a[9]}
-echo "Tokens set"
+
+echo "got tokens"
+
 echo "Secure Tunnel Destination test"
 ./secure-tunnel --region $REGION --access_token $DESTINATIONTOKEN
 
 echo "Secure Tunnel Source test"
-./secure-tunnel --region $REGION --access_token $SOURCETOKEN --localProxyModeSource
+./secure-tunnel --localProxyModeSource --region $REGION --access_token $SOURCETOKEN
