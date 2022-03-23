7c0306c398b92e883efb6723730cad5b4fd26997
Vera Xia <zhvxia@amazon.com>
2022-03-23 16:17:56 -0700

    move skip sample to builder variant

--- .builder/actions/build_samples.py
+++ .builder/actions/build_samples.py
@@ -5,15 +5,6 @@ import sys
 
 class BuildSamples(Builder.Action):
     def run(self, env):
-        skip_da = False
-        skip_samples = False
-        if env.args.cli_config['variables'].get('skip_da', "0") != "0":
-            skip_da = True
-            print('skip_da is defined. Skipping device advisor samples...')
-        if env.args.cli_config['variables'].get('skip_samples', "0") != "0":
-            skip_samples = True
-            print('skip_samples is defined. Skipping samples...')
-
         steps = []
         samples = [
             'samples/mqtt/basic_pub_sub',
@@ -33,28 +24,26 @@ class BuildSamples(Builder.Action):
             'deviceadvisor/tests/shadow_update'
         ]
 
-        if skip_samples == False :
-            for sample_path in samples:
-                build_path = os.path.join('build', sample_path)
-                steps.append(['cmake',
-                              f'-B{build_path}',
-                              f'-H{sample_path}',
-                              f'-DCMAKE_PREFIX_PATH={env.install_dir}',
-                             '-DCMAKE_BUILD_TYPE=RelWithDebInfo'])
-                steps.append(['cmake',
-                          '--build', build_path,
-                          '--config', 'RelWithDebInfo'])
-            
-        if skip_da == False :
-            for sample_path in da_samples:
-                build_path = os.path.join('build', sample_path)
-                steps.append(['cmake',
+        for sample_path in samples:
+            build_path = os.path.join('build', sample_path)
+            steps.append(['cmake',
                             f'-B{build_path}',
                             f'-H{sample_path}',
                             f'-DCMAKE_PREFIX_PATH={env.install_dir}',
                             '-DCMAKE_BUILD_TYPE=RelWithDebInfo'])
-                steps.append(['cmake',
-                            '--build', build_path,
-                            '--config', 'RelWithDebInfo'])
+            steps.append(['cmake',
+                        '--build', build_path,
+                        '--config', 'RelWithDebInfo'])
+        
+        for sample_path in da_samples:
+            build_path = os.path.join('build', sample_path)
+            steps.append(['cmake',
+                        f'-B{build_path}',
+                        f'-H{sample_path}',
+                        f'-DCMAKE_PREFIX_PATH={env.install_dir}',
+                        '-DCMAKE_BUILD_TYPE=RelWithDebInfo'])
+            steps.append(['cmake',
+                        '--build', build_path,
+                        '--config', 'RelWithDebInfo'])
 
         return Builder.Script(steps)
