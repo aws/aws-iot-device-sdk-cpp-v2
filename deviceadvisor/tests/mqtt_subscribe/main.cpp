2024994e12ed99060adbf27d24550969238620fa
xiazhvera <zhvxia@amazon.com>
2022-06-03 15:11:09 -0700

    Delete Test Resources for Device Advisor (#441)
    * make sure connect with clean session for da testing
    
    * If the test failed, uploaded the logs and delete the test thing
    
    * add backoff for each device advisor request
    
    * extend connect pingtimeout for device advisor test apps
    
    * extend connect pingtimeout for device advisor test apps
    
    * add output for log file path
    
    * wait for subscription finished
    
    * fix format

--- deviceadvisor/tests/mqtt_subscribe/main.cpp
+++ deviceadvisor/tests/mqtt_subscribe/main.cpp
@@ -68,6 +68,7 @@ int main()
      * Invoked when connection and disconnection has completed.
      */
     std::promise<bool> connectionCompletedPromise;
+    std::promise<void> subscriptionCompletedPromise;
     std::promise<void> connectionClosedPromise;
 
     connection->OnConnectionCompleted = [&](Mqtt::MqttConnection &, int errorCode, Mqtt::ReturnCode returnCode, bool) {
@@ -79,7 +80,7 @@ int main()
      * Actually perform the connect dance.
      */
     if (!connection->Connect(
-            clientId.c_str(), false /*cleanSession*/, 1000 /*keepAliveTimeSecs*/, 6000 /*pingTimeoutMs*/))
+            clientId.c_str(), true /*cleanSession*/, 1000 /*keepAliveTimeSecs*/, 60000 /*pingTimeoutMs*/))
     {
         exit(-1);
     }
@@ -96,15 +97,16 @@ int main()
                              const ByteBuf & /*byteBuf*/,
                              bool /*dup*/,
                              Mqtt::QOS /*qos*/,
-                             bool /*retain*/) {};
+                             bool /*retain*/) { subscriptionCompletedPromise.set_value(); };
 
         auto onSubAck = [&](Mqtt::MqttConnection &,
                             uint16_t /*packetId*/,
                             const String & /*topic*/,
                             Mqtt::QOS /*QoS*/,
-                            int /*errorCode*/) {};
+                            int /*errorCode*/) { subscriptionCompletedPromise.set_value(); };
 
         connection->Subscribe(daVars.topic.c_str(), AWS_MQTT_QOS_AT_MOST_ONCE, onMessage, onSubAck);
+        subscriptionCompletedPromise.get_future().wait();
 
         /* Disconnect */
         if (connection->Disconnect())
