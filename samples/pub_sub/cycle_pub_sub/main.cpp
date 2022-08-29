c89e7c1878991a5ea6b406ea16c9096a9d55b2d9
Noah Beard <ncbeard@amazon.com>
2022-08-29 16:09:53 -0400

    Improve cycle_pub_sub sample: * Correctly wait for connection, disconnection, subscribe, and unsubscribe * Add documentation to functions and tidy code up a bit * Seed random number generator so it does not always run the same way each time * Clang-format everything

--- samples/pub_sub/cycle_pub_sub/main.cpp
+++ samples/pub_sub/cycle_pub_sub/main.cpp
@@ -18,41 +18,99 @@
 
 using namespace Aws::Crt;
 
+/**
+ * A struct to hold all of the data for each MQTT connection in the
+ * sample, like the client ID, whether it is connected, etc.
+ */
 struct CycleClient
 {
     std::shared_ptr<Aws::Crt::Mqtt::MqttConnection> client;
-    // TODO - add a way to tell if we are connected or not via promises or similar. Right now is kind of unsafe but it
-    // works
     String shared_topic = "test/shared_topic";
     String client_id = "";
     String publish_payload = "Hello World!";
     bool is_connected = false;
-    bool subscribed_to_topics = false;
+    bool is_subscribed_to_topics = false;
 };
 
+/* A vector to hold all the clients used in the sample */
+std::vector<CycleClient> clients_holder;
+
+/**
+ * A helper function to get the CycleClient struct associated with the passed-in MqttConnection.
+ * This allows us to get the CycleClient data in the MQTT callbacks.
+ *
+ * @param clients_holder The vector containing all the CycleClients
+ * @param connection The MqttConnection to find the CycleClient for
+ * @return The CycleClient associated with the MqttConnection or nullptr if none is found
+ */
+CycleClient *getClientFromConnection(
+    std::vector<CycleClient> *clients_holder,
+    Aws::Crt::Mqtt::MqttConnection *connection)
+{
+    for (int i = 0; i < clients_holder->size(); i++)
+    {
+        CycleClient *current_client = &clients_holder->at(i);
+        if (current_client->client.get() == connection)
+        {
+            return current_client;
+        }
+    }
+    return nullptr;
+}
+
+/**
+ * A helper function that sets up the passed-in empty CycleClient struct.
+ *
+ * @param empty_client The CycleClient struct to set up
+ * @param index The index of the CycleClient in the vector containing all the CycleClients
+ * @param cmd_utils The CommandLineUtils used in the sample - for creating a MQTT connection
+ */
 void createNewClient(CycleClient *empty_client, size_t index, Utils::CommandLineUtils *cmd_utils)
 {
     empty_client->client = cmd_utils->BuildMQTTConnection();
     empty_client->client_id = "test-" + Aws::Crt::UUID().ToString() + "-client-";
     empty_client->client_id.append(std::to_string(index).c_str());
 
-    auto onConnectionCompleted = [&](Mqtt::MqttConnection &, int errorCode, Mqtt::ReturnCode returnCode, bool) {
-        if (errorCode)
-        {
-            fprintf(stdout, "[Lifecycle] Connection failed with error %s\n", ErrorDebugString(errorCode));
-            exit(-1);
-        }
-        else
-        {
-            fprintf(stdout, "[Lifecycle] Connection completed with return code %d\n", returnCode);
-        }
-    };
+    auto onConnectionCompleted =
+        [&](Mqtt::MqttConnection &connection, int errorCode, Mqtt::ReturnCode returnCode, bool) {
+            if (errorCode)
+            {
+                fprintf(stdout, "[Lifecycle] Connection failed with error %s\n", ErrorDebugString(errorCode));
+                exit(-1);
+            }
+            else
+            {
+                fprintf(stdout, "[Lifecycle] Connection completed with return code %d\n", returnCode);
+
+                CycleClient *current_client = getClientFromConnection(&clients_holder, &connection);
+                if (current_client != nullptr)
+                {
+                    current_client->is_connected = true;
+                }
+                else
+                {
+                    fprintf(stdout, "[Lifecycle] Connection could not find client in vector! Quitting sample");
+                    exit(-1);
+                }
+            }
+        };
     auto onInterrupted = [&](Mqtt::MqttConnection &, int error) {
         fprintf(stdout, "[Lifecycle] Connection interrupted with error %s\n", ErrorDebugString(error));
     };
-    auto onDisconnect = [&](Mqtt::MqttConnection &) {
+    auto onDisconnect = [&](Mqtt::MqttConnection &connection) {
         {
             fprintf(stdout, "[Lifecycle] Disconnect completed\n");
+
+            CycleClient *current_client = getClientFromConnection(&clients_holder, &connection);
+            if (current_client != nullptr)
+            {
+                current_client->is_connected = false;
+            }
+            else
+            {
+                fprintf(stdout, "[Lifecycle] Disconnection could not find client in vector! Quitting sample");
+                exit(-1);
+            }
         }
     };
     auto onResumed = [&](Mqtt::MqttConnection &, Mqtt::ReturnCode, bool) {
@@ -65,6 +123,9 @@ void createNewClient(CycleClient *empty_client, size_t index, Utils::CommandLine
     empty_client->client->OnConnectionResumed = std::move(onResumed);
 }
 
+/**
+ * All of the operations the MQTT clients can perform in the sample
+ */
 enum OPERATIONS
 {
     OPERATION_NULL,
@@ -81,6 +142,12 @@ enum OPERATIONS
     OPERATION_LENGTH
 };
 
+/**
+ * The null operation - does nothing.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationNull(CycleClient *current_client, int index)
 {
     (void)current_client;
@@ -88,6 +155,12 @@ void operationNull(CycleClient *current_client, int index)
     fprintf(stdout, "[OP] Null called for client %i\n", index);
 }
 
+/**
+ * The start operation will connect the passed-in client, if it is not already connected.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationStart(CycleClient *current_client, int index)
 {
     if (current_client->is_connected == true)
@@ -107,10 +180,28 @@ void operationStart(CycleClient *current_client, int index)
         current_client->is_connected = false;
         exit(-1);
     }
-    current_client->is_connected = true;
+
+    fprintf(stdout, "[OP] Waiting a maximum of 1 minute for client %i to report it is connected\n", index);
+    int wait_count = 0;
+    while (current_client->is_connected == false)
+    {
+        wait_count += 1;
+        if (wait_count >= 60)
+        {
+            fprintf(stdout, "[OP] Client %i did not report connected after 1 minute\n", index);
+            exit(-1);
+        }
+        std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+    }
     fprintf(stdout, "[OP] Started client %i\n", index);
 }
 
+/**
+ * The stop operation will disconnect the passed-in client, if it is not already disconnected.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationStop(CycleClient *current_client, int index)
 {
     if (current_client->is_connected == false)
@@ -122,8 +213,19 @@ void operationStop(CycleClient *current_client, int index)
 
     if (current_client->client->Disconnect())
     {
+        fprintf(stdout, "[OP] Waiting a maximum of 1 minute for client %i to report it is disconnected\n", index);
+        int wait_count = 0;
+        while (current_client->is_connected == true)
+        {
+            wait_count += 1;
+            if (wait_count >= 60)
+            {
+                fprintf(stdout, "[OP] Client %i did not report connected after 1 minute\n", index);
+                exit(-1);
+            }
+            std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+        }
         fprintf(stdout, "[OP] Stopped client %i\n", index);
-        current_client->is_connected = false;
     }
     else
     {
@@ -131,6 +233,13 @@ void operationStop(CycleClient *current_client, int index)
     }
 }
 
+/**
+ * The subscribe operation will subscribe to the shared topic and the client Id
+ * topic of the passed-in client, if not already subscribed.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationSubscribe(CycleClient *current_client, int index)
 {
     if (current_client->is_connected == false)
@@ -138,7 +247,7 @@ void operationSubscribe(CycleClient *current_client, int index)
         operationStart(current_client, index);
         return;
     }
-    if (current_client->subscribed_to_topics == true)
+    if (current_client->is_subscribed_to_topics == true)
     {
         fprintf(stdout, "[OP] Client %i already subscribed\n", index);
         return;
@@ -159,7 +268,12 @@ void operationSubscribe(CycleClient *current_client, int index)
         }
     };
 
-    auto onSubAck = [&](Mqtt::MqttConnection &, uint16_t packetId, const String &topic, Mqtt::QOS QoS, int errorCode) {
+    std::promise<void> subscribeFinishedPromise;
+    auto onSubAck = [&](Mqtt::MqttConnection &connection,
+                        uint16_t packetId,
+                        const String &topic,
+                        Mqtt::QOS QoS,
+                        int errorCode) {
         if (errorCode)
         {
             fprintf(stderr, "[Lifecycle] Subscribe failed with error %s\n", aws_error_debug_str(errorCode));
@@ -176,20 +290,31 @@ void operationSubscribe(CycleClient *current_client, int index)
             {
                 fprintf(
                     stdout, "[Lifecycle] Subscribe on topic %s on packetId %d Succeeded\n", topic.c_str(), packetId);
+                subscribeFinishedPromise.set_value();
             }
         }
     };
 
     current_client->client->Subscribe(
         current_client->client_id.c_str(), AWS_MQTT_QOS_AT_LEAST_ONCE, onMessage, onSubAck);
+    subscribeFinishedPromise.get_future().wait();
+
+    subscribeFinishedPromise = std::promise<void>();
     current_client->client->Subscribe(
         current_client->shared_topic.c_str(), AWS_MQTT_QOS_AT_LEAST_ONCE, onMessage, onSubAck);
-    // Not sure how to wait for subscribe, so... don't I guess...
+    subscribeFinishedPromise.get_future().wait();
 
-    current_client->subscribed_to_topics = true;
+    current_client->is_subscribed_to_topics = true;
     fprintf(stdout, "[OP] Subscribed client %i\n", index);
 }
 
+/**
+ * The unsubscribe operation will unsubscribe to the shared topic and the client Id
+ * topic of the passed-in client, if not already unsubscribed.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationUnsubscribe(CycleClient *current_client, int index)
 {
     if (current_client->is_connected == false)
@@ -197,30 +322,38 @@ void operationUnsubscribe(CycleClient *current_client, int index)
         operationStart(current_client, index);
         return;
     }
-    if (current_client->subscribed_to_topics == false)
+    if (current_client->is_subscribed_to_topics == false)
     {
         fprintf(stdout, "[OP] Client %i not subscribed\n", index);
         return;
     }
     fprintf(stdout, "[OP] About to unsubscribe client %i\n", index);
 
-    std::promise<void> unsubscribeFinishedPromise_One;
+    std::promise<void> unsubscribeFinishedPromise;
     current_client->client->Unsubscribe(current_client->client_id.c_str(), [&](Mqtt::MqttConnection &, uint16_t, int) {
-        unsubscribeFinishedPromise_One.set_value();
+        unsubscribeFinishedPromise.set_value();
     });
-    unsubscribeFinishedPromise_One.get_future().wait();
+    unsubscribeFinishedPromise.get_future().wait();
 
-    std::promise<void> unsubscribeFinishedPromise_Two;
+    unsubscribeFinishedPromise = std::promise<void>();
     current_client->client->Unsubscribe(
         current_client->shared_topic.c_str(),
-        [&](Mqtt::MqttConnection &, uint16_t, int) { unsubscribeFinishedPromise_Two.set_value(); });
-    unsubscribeFinishedPromise_Two.get_future().wait();
+        [&](Mqtt::MqttConnection &, uint16_t, int) { unsubscribeFinishedPromise.set_value(); });
+    unsubscribeFinishedPromise.get_future().wait();
 
-    current_client->subscribed_to_topics = false;
+    current_client->is_subscribed_to_topics = false;
 
     fprintf(stdout, "[OP] Unsubscribed client %i\n", index);
 }
 
+/**
+ * The publish operation will publish a payload to the passed-in topic using the passed-in QOS.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ * @param QOS The QOS to use with the publish
+ * @param topic The topic to publish the message to
+ */
 void operationPublish(CycleClient *current_client, int index, Aws::Crt::Mqtt::QOS QOS, String topic)
 {
     if (current_client->is_connected == false)
@@ -239,36 +372,83 @@ void operationPublish(CycleClient *current_client, int index, Aws::Crt::Mqtt::QO
     fprintf(stdout, "[OP] Published client %i with QoS %i with topic %s\n", index, QOS, topic.c_str());
 }
 
+/**
+ * The publish QOS 0 operation will publish a QOS 0 message to the "topic1" topic
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationPublishQOS0(CycleClient *current_client, int index)
 {
     operationPublish(current_client, index, AWS_MQTT_QOS_AT_MOST_ONCE, "topic1");
 }
 
+/**
+ * The publish QOS 1 operation will publish a QOS 1 message to the "topic1" topic
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationPublishQOS1(CycleClient *current_client, int index)
 {
     operationPublish(current_client, index, AWS_MQTT_QOS_AT_LEAST_ONCE, "topic1");
 }
 
+/**
+ * The publish to subscribed QOS 0 operation will publish a QOS 0 message to the
+ * client Id topic.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationPublishToSubscribedTopicQOS0(CycleClient *current_client, int index)
 {
     operationPublish(current_client, index, AWS_MQTT_QOS_AT_MOST_ONCE, current_client->client_id);
 }
 
+/**
+ * The publish to subscribed QOS 1 operation will publish a QOS 1 message to the
+ * client Id topic.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationPublishToSubscribedTopicQOS1(CycleClient *current_client, int index)
 {
     operationPublish(current_client, index, AWS_MQTT_QOS_AT_LEAST_ONCE, current_client->client_id);
 }
 
+/**
+ * The publish to shared QOS 0 operation will publish a QOS 0 message to the
+ * shared topic that all clients can be subscribed to.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationPublishToSharedTopicQOS0(CycleClient *current_client, int index)
 {
     operationPublish(current_client, index, AWS_MQTT_QOS_AT_MOST_ONCE, current_client->shared_topic);
 }
 
+/**
+ * The publish to shared QOS 1 operation will publish a QOS 1 message to the
+ * shared topic that all clients can be subscribed to.
+ *
+ * @param current_client The client to operform the operation on
+ * @param index The index of the client passed
+ */
 void operationPublishToSharedTopicQOS1(CycleClient *current_client, int index)
 {
     operationPublish(current_client, index, AWS_MQTT_QOS_AT_LEAST_ONCE, current_client->shared_topic);
 }
 
+/**
+ * Performs the operation for the passed-in random index on the passed-in CycleClient.
+ *
+ * @param current_client The client to perform the operation on
+ * @param index The index of the client passed
+ * @param random_index The index of the operation to perform
+ */
 void performOperation(CycleClient *current_client, int index, int random_index)
 {
     if (random_index == OPERATION_NULL)
@@ -322,10 +502,15 @@ void performOperation(CycleClient *current_client, int index, int random_index)
     }
 }
 
-void performRandomOperation(std::vector<CycleClient> *clients_holder, size_t length)
+/**
+ * Iterates through the vector holding all of the clients and performs a random operation
+ *
+ * @param clients_holder A vector containing all of the clients
+ */
+void performRandomOperation(std::vector<CycleClient> *clients_holder)
 {
     int random_index = rand() % OPERATION_LENGTH;
-    for (size_t i = 0; i < length; i++)
+    for (size_t i = 0; i < clients_holder->size(); i++)
     {
         performOperation(&clients_holder->at(i), (int)i, random_index);
         random_index = rand() % OPERATION_LENGTH;
@@ -343,8 +528,7 @@ int main(int argc, char *argv[])
 
     uint32_t config_clients = 3;
     uint32_t config_tps = 12;
-    // uint64_t config_seconds = 600;
-    uint64_t config_seconds = 120;
+    uint64_t config_seconds = 300;
 
     /*********************** Parse Arguments ***************************/
     Utils::CommandLineUtils cmdUtils = Utils::CommandLineUtils();
@@ -352,12 +536,11 @@ int main(int argc, char *argv[])
     cmdUtils.AddCommonMQTTCommands();
     cmdUtils.RegisterCommand("key", "<path>", "Path to your key in PEM format.");
     cmdUtils.RegisterCommand("cert", "<path>", "Path to your client certificate in PEM format.");
-
     cmdUtils.RegisterCommand("clients", "<int>", "The number of clients/connections to make (optional, default='3'");
     cmdUtils.RegisterCommand(
         "tps", "<int>", "The number of seconds to wait after performing an operation (optional, default=12)");
     cmdUtils.RegisterCommand(
-        "seconds", "<int>", "The number of seconds to run the sample for (optional, default='600')");
+        "seconds", "<int>", "The number of seconds to run the sample for (optional, default='300')");
 
     cmdUtils.AddLoggingCommands();
     const char **const_argv = (const char **)argv;
@@ -394,7 +577,6 @@ int main(int argc, char *argv[])
     /******************** Start the client cycle ***********************/
 
     // Make the clients
-    std::vector<CycleClient> clients_holder;
     for (size_t i = 0; i < config_clients; i++)
     {
         clients_holder.push_back(CycleClient());
@@ -404,6 +586,9 @@ int main(int argc, char *argv[])
 
     // Get the current time
     auto startTime = std::chrono::steady_clock::now();
+
+    // Seed the random number generator with the current time
+    srand(time(nullptr));
 
     // Start the loop
     bool done = false;
@@ -418,20 +603,18 @@ int main(int argc, char *argv[])
         {
             done = true;
         }
-        performRandomOperation(&clients_holder, config_clients);
-
+        performRandomOperation(&clients_holder);
         std::this_thread::sleep_for(std::chrono::milliseconds(1000 * config_tps));
     }
 
     /*************************** Clean up ******************************/
 
-    // Stop all the clients
+    // Stop all the clients and clear the vector
     for (size_t i = 0; i < config_clients; i++)
     {
         operationStop(&clients_holder.at(i), (int)i);
     }
-    // Wait for a couple seconds
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    clients_holder.clear();
 
     fprintf(stdout, "\nCycle pub-sub sample complete\n\n");
     return 0;
