6552a2ac79bbad7836461d88c3ab51d6b1084d46
Steve Kim <sbstevek@amazon.com>
2023-04-10 14:19:57 -0700

    open and store tunnel tokens

--- codebuild/integration-tests.sh
+++ codebuild/integration-tests.sh
@@ -4,11 +4,16 @@ env
 
 pushd $CODEBUILD_SRC_DIR/secure_tunneling/tests
 
+printf "Currently set env variables\n"
 printenv
 
-aws iotsecuretunneling open-tunnel --destination-config services=ssh,ssh2,ssh3 --timeout-config maxLifetimeTimeoutMinutes=10
+tunnel_info=$(aws iotsecuretunneling open-tunnel --destination-config services=ssh,ssh2,ssh3 --timeout-config maxLifetimeTimeoutMinutes=10) && echo -e "$tunnel_info" > /tmp/tunnel_info.pem
+source_access_token=$(sed '4!d' /tmp/tunnel_info.pem | cut -d'"' -f4) && echo -e "$source_access_token" > /tmp/source_access_token.pem
+destination_access_token=$(sed '5!d' /tmp/tunnel_info.pem | cut -d'"' -f4) && echo -e "$destination_access_token" > /tmp/destination_access_token.pem
 
-
+cat /tmp/tunnel_info.pem
+cat /tmp/source_access_token.pem
+cat /tmp/destination_access_token.pem
 
 # mkdir _build
 # cd _build
