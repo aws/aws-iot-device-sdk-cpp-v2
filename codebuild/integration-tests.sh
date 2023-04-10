686f20f20a20aad0c880ad9b13429d83439aaf36
Steve Kim <sbstevek@amazon.com>
2023-04-10 10:26:50 -0700

    open secure tunnel

--- codebuild/integration-tests.sh
+++ codebuild/integration-tests.sh
@@ -6,7 +6,7 @@ pushd $CODEBUILD_SRC_DIR/secure_tunneling/tests
 
 printenv
 
-aws iotsecuretunneling open-tunnel --destination-config thingName=,services=ssh --timeout-config maxLifeTimeOutMinutes = 10
+aws iotsecuretunneling open-tunnel --destination-config services=ssh,ssh2,ssh3 --timeout-config maxLifetimeTimeoutMinutes=10
 
 
 
