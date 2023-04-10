7f344283e486f537580e2e3b303c6d20e7edf6d3
Steve Kim <sbstevek@amazon.com>
2023-04-10 10:09:59 -0700

    check what's set

--- codebuild/integration-tests.sh
+++ codebuild/integration-tests.sh
@@ -1,8 +1,12 @@
+set -e
+
 env
 
 pushd $CODEBUILD_SRC_DIR/secure_tunneling/tests
 
 aws iotsecuretunneling open-tunnel --destination-config thingName=,services=ssh --timeout-config maxLifeTimeOutMinutes = 10
+
+printenv
 
 # mkdir _build
 # cd _build
