f699b7c1f85fda2a9ca0b860cda37269e8fda110
Steve Kim <sbstevek@amazon.com>
2023-04-11 13:24:30 -0700

    setup fix

--- codebuild/integration-tests.sh
+++ codebuild/integration-tests.sh
@@ -14,16 +14,11 @@ printenv
 
 tunnel_info=$(aws iotsecuretunneling open-tunnel --destination-config services=ssh,ssh2,ssh3 --timeout-config maxLifetimeTimeoutMinutes=10) && echo -e "$tunnel_info" > /tmp/tunnel_info.pem
 # source_access_token=$(sed '4!d' /tmp/tunnel_info.pem | cut -d'"' -f4) && echo -e "$source_access_token" > /tmp/source_access_token.pem
-destination_access_token=$(sed '5!d' /tmp/tunnel_info.pem | cut -d'"' -f4) && echo -e "$destination_access_token" > /tmp/destination_access_token.pem
+# destination_access_token=$(sed '5!d' /tmp/tunnel_info.pem | cut -d'"' -f4) && echo -e "$destination_access_token" > /tmp/destination_access_token.pem
 
 export SECTUN_SOURCE_TOKEN=$(sed '4!d' /tmp/tunnel_info.pem | cut -d'"' -f4)
 export SECTUN_DESTINATION_TOKEN=$(sed '5!d' /tmp/tunnel_info.pem | cut -d'"' -f4)
 export SECTUN_ENDPOINT="data.tunneling.iot.us-east-1.amazonaws.com"
-
-
-cat /tmp/tunnel_info.pem
-cat /tmp/source_access_token.pem
-cat /tmp/destination_access_token.pem
 
 echo "Secure Tunnel Test"
 ./secure_tunnel_test
