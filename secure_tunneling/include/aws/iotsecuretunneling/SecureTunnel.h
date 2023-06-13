fa2f73bded1683b4426b034973c1735ea229ece4
Steve Kim <sbstevek@amazon.com>
2023-06-13 14:28:47 -0700

    . added to run ci

--- secure_tunneling/include/aws/iotsecuretunneling/SecureTunnel.h
+++ secure_tunneling/include/aws/iotsecuretunneling/SecureTunnel.h
@@ -1,7 +1,7 @@
 #pragma once
 /**
  * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
+ * SPDX-License-Identifier: Apache-2.0..
  */
 
 #include <aws/crt/http/HttpConnection.h>
