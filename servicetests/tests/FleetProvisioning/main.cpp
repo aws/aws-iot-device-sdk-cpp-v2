ca9f0b3bd30ec832b798fcae8f2a6e9daca1a7f7
Alfred Gedeon <alfred2g@hotmail.com>
2023-12-05 07:56:08 -0800

    Fill up the main function

--- servicetests/tests/FleetProvisioning/main.cpp
+++ servicetests/tests/FleetProvisioning/main.cpp
@@ -1,5 +1,55 @@
+/**
+ * Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+ * SPDX-License-Identifier: Apache-2.0.
+ */
+#include <aws/crt/Api.h>
+#include <aws/crt/JsonObject.h>
+#include <aws/crt/UUID.h>
+#include <aws/crt/io/HostResolver.h>
+
+#include <aws/iot/MqttClient.h>
+
+#include <aws/iotidentity/CreateCertificateFromCsrRequest.h>
+#include <aws/iotidentity/CreateCertificateFromCsrResponse.h>
+#include <aws/iotidentity/CreateCertificateFromCsrSubscriptionRequest.h>
+#include <aws/iotidentity/CreateKeysAndCertificateRequest.h>
+#include <aws/iotidentity/CreateKeysAndCertificateResponse.h>
+#include <aws/iotidentity/CreateKeysAndCertificateSubscriptionRequest.h>
+#include <aws/iotidentity/ErrorResponse.h>
+#include <aws/iotidentity/IotIdentityClient.h>
+#include <aws/iotidentity/RegisterThingRequest.h>
+#include <aws/iotidentity/RegisterThingResponse.h>
+#include <aws/iotidentity/RegisterThingSubscriptionRequest.h>
+
+#include <algorithm>
+#include <chrono>
+#include <condition_variable>
+#include <fstream>
+#include <iostream>
+#include <mutex>
+#include <sstream>
+#include <streambuf>
+#include <string>
+#include <thread>
+
+#include "../../../samples/utils/CommandLineUtils.h"
 
 int main(int argc, char * argv[])
 {
+    // Do the global initialization for the API
+    ApiHandle apiHandle;
+
+    /**
+     * cmdData is the arguments/input from the command line placed into a single struct for
+     * use in this sample. This handles all of the command line parsing, validating, etc.
+     * See the Utils/CommandLineUtils for more information.
+     */
+    Utils::cmdData cmdData = Utils::parseSampleInputJobs(argc, argv, &apiHandle);
+
+    /**
+     * In a real world application you probably don't want to enforce synchronous behavior
+     * but this is a sample console application, so we'll just do that with a condition variable.
+     */
+
     return 0;
 }
