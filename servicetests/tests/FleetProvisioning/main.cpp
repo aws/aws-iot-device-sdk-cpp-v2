120dd92eee39c4e64d4279b98fbf6ce656a40fea
Alfred Gedeon <alfred2g@hotmail.com>
2023-12-07 10:27:06 -0800

    Change argument parsing method

--- servicetests/tests/FleetProvisioning/main.cpp
+++ servicetests/tests/FleetProvisioning/main.cpp
@@ -473,7 +473,7 @@ int main(int argc, char *argv[])
      * use in this sample. This handles all of the command line parsing, validating, etc.
      * See the Utils/CommandLineUtils for more information.
      */
-    Utils::cmdData cmdData = Utils::parseSampleInputJobs(argc, argv, &apiHandle);
+    Utils::cmdData cmdData = Utils::parseSampleInputFleetProvisioning(argc, argv, &apiHandle);
 
     /**
      * In a real world application you probably don't want to enforce synchronous behavior
