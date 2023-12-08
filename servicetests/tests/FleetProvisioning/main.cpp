5cb7324084e969c85af8e98c888c3c0b08c55b48
Alfred Gedeon <alfred2g@hotmail.com>
2023-12-08 08:27:11 -0800

    wait forever

--- servicetests/tests/FleetProvisioning/main.cpp
+++ servicetests/tests/FleetProvisioning/main.cpp
@@ -312,12 +312,15 @@ void createKeysAndCertificateWorkflow(
     }
     onSubAckPromise = std::promise<void>();
 
-    status = gotResponse.get_future().wait_for(span); // 5 seconds
+    //status = gotResponse.get_future().wait_for(span); // 5 seconds
+    gotResponse.get_future().wait(); // 5 seconds
+    /*
     if (status == std::future_status::timeout)
     {
         fprintf(stderr, "Error: - gotResponse timeout %s\n", __FUNCTION__);
         exit(-1);
     }
+    */
     gotResponse = std::promise<void>();
 
     auto rejectedHandler = [&](Aws::Iotidentity::ErrorResponse *error, int ioErr) {
