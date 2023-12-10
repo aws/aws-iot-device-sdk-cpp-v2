b8fa15a252c464d7e4eb88c10742e807c770b4e8
Alfred Gedeon <alfred2g@hotmail.com>
2023-12-10 08:56:42 -0800

    fix syntax

--- servicetests/test_cases/test_shadow_update.py
+++ servicetests/test_cases/test_shadow_update.py
@@ -106,12 +106,12 @@ def main():
 
     # Delete a thing created for this test run.
     # NOTE We want to try to delete thing even if test was unsuccessful.
-    try:
+    #try:
        # ci_iot_thing.delete_iot_thing(thing_name, parsed_commands.region)
-    except Exception as e:
-        print(f"ERROR: Failed to delete thing: {e}")
+    #except Exception as e:
+    #    print(f"ERROR: Failed to delete thing: {e}")
         # Fail the test if unable to delete thing, so this won't remain unnoticed.
-        test_result = -1
+    #    test_result = -1
 
     try:
         if os.path.isfile(certificate_path):
