27823dabe73defcfd799349f9cedfa2114c6092a
Igor Abdrakhimov <igorabd@amazon.com>
2024-03-07 11:24:22 -0800

    Revert secure tunneling

--- secure_tunneling/include/aws/iotsecuretunneling/Exports.h
+++ secure_tunneling/include/aws/iotsecuretunneling/Exports.h
@@ -1,9 +1,19 @@
 #pragma once
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- *
- * This file is generated
- */
+/* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
+
+* This file is generated
+*/
 
 #if defined(AWS_IOTSECURETUNNELING_USE_WINDOWS_DLL_SEMANTICS) || defined(WIN32)
 #    ifdef AWS_IOTSECURETUNNELING_USE_IMPORT_EXPORT
@@ -18,4 +28,4 @@
 
 #else /* defined (AWS_IOTSECURETUNNELING_USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
 #    define AWS_IOTSECURETUNNELING_API
-#endif /* defined (AWS_IOTSECURETUNNELING__USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
+#endif /* defined (AWS_IOTSECURETUNNELING_USE_WINDOWS_DLL_SEMANTICS) || defined (WIN32) */
--- secure_tunneling/include/aws/iotsecuretunneling/SubscribeToTunnelsNotifyRequest.h
+++ secure_tunneling/include/aws/iotsecuretunneling/SubscribeToTunnelsNotifyRequest.h
@@ -1,10 +1,19 @@
 #pragma once
+/* Copyright 2010-2019 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+*
+* Licensed under the Apache License, Version 2.0 (the "License").
+* You may not use this file except in compliance with the License.
+* A copy of the License is located at
+*
+*  http://aws.amazon.com/apache2.0
+*
+* or in the "license" file accompanying this file. This file is distributed
+* on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+* express or implied. See the License for the specific language governing
+* permissions and limitations under the License.
 
-/* Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
- * SPDX-License-Identifier: Apache-2.0.
- *
- * This file is generated
- */
+* This file is generated
+*/
 
 #include <aws/iotsecuretunneling/Exports.h>
 
@@ -16,10 +25,6 @@ namespace Aws
     namespace Iotsecuretunneling
     {
 
-        /**
-         * Data needed to subscribe to the secure tunnel notify topic
-         *
-         */
         class AWS_IOTSECURETUNNELING_API SubscribeToTunnelsNotifyRequest final
         {
           public:
@@ -30,10 +35,6 @@ namespace Aws
 
             void SerializeToObject(Crt::JsonObject &doc) const;
 
-            /**
-             * Name of the AWS IoT thing that is making the subscription request
-             *
-             */
             Aws::Crt::Optional<Aws::Crt::String> ThingName;
 
           private:
