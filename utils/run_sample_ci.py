1ffefd572c2ad221b98e9f4e01f3dce91f00c853
Igor Abdrakhimov <igorabd@amazon.com>
2024-04-30 11:45:51 -0700

    Remove extra params in running samples

--- utils/run_sample_ci.py
+++ utils/run_sample_ci.py
@@ -290,7 +290,7 @@ def launch_sample():
     elif (config_json['language'] == "CPP"):
         try:
             sample_return = subprocess.run(
-                args=config_json_arguments_list, executable=config_json['sample_file'], timeout=600, stderr=subprocess.STDOUT, stdout=subprocess.PIPE)
+                args=config_json_arguments_list, executable=config_json['sample_file'], timeout=600)
             exit_code = sample_return.returncode
         except subprocess.TimeoutExpired as timeOut:
             sys.exit(-1)
