4ba3b9a39b1643b2f4b348fcf49f968b4a772ec2
Igor Abdrakhimov <igorabd@amazon.com>
2024-05-16 11:19:38 -0700

    Add new builder variant for GG only

---
+++ .builder/actions/build_gg_samples.py
@@ -0,0 +1,30 @@
+import Builder
+import os
+import sys
+import argparse
+
+
+class BuildGGSamples(Builder.Action):
+    def run(self, env):
+        # parse extra cmake configs
+        cmd_args = env.args
+
+        steps = []
+        samples = [
+            'samples/greengrass/ipc',
+        ]
+
+        for sample_path in samples:
+            build_path = os.path.join('build', sample_path)
+            steps.append(['cmake',
+                          f'-B{build_path}',
+                          f'-H{sample_path}',
+                          f'-DCMAKE_PREFIX_PATH={env.install_dir}',
+                          '-DCMAKE_BUILD_TYPE=RelWithDebInfo'])
+            # append extra cmake configs
+            steps[-1].extend(cmd_args.cmake_extra)
+            steps.append(['cmake',
+                          '--build', build_path,
+                          '--config', 'RelWithDebInfo'])
+
+        return Builder.Script(steps)
