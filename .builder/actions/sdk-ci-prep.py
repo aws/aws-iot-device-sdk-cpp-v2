421baabf0fa17033095554188d74b8412ea031dd
Bret Ambrose <bretambrose@gmail.com>
2024-11-11 11:39:56 -0800

    Does the build have credentials injected?

--- .builder/actions/sdk-ci-prep.py
+++ .builder/actions/sdk-ci-prep.py
@@ -1,7 +1,12 @@
 import Builder
+import os
 
 class SdkCiPrep(Builder.Action):
     def run(self, env):
+        if os.environ.get('AWS_ACCESS_KEY_ID') != None:
+            print(f"Local credentials exist")
+        else:
+            print(f"No Local credentials")
         actions = [
             Builder.SetupCrossCICrtEnvironment()
         ]
