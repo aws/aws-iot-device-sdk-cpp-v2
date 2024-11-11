f04dfd6f6abcf2cd4a1365405223bd3167167ff2
Bret Ambrose <bretambrose@gmail.com>
2024-11-11 11:50:39 -0800

    Check for session

--- .builder/actions/sdk-ci-prep.py
+++ .builder/actions/sdk-ci-prep.py
@@ -5,8 +5,12 @@ class SdkCiPrep(Builder.Action):
     def run(self, env):
         if os.environ.get('AWS_ACCESS_KEY_ID') != None:
             print(f"Local credentials exist")
+            if os.environ.get('AWS_SESSION_TOKEN') != None:
+                print(f"Local credentials are session based")
         else:
             print(f"No Local credentials")
+
+
         actions = [
             Builder.SetupCrossCICrtEnvironment()
         ]
