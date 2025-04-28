6a800a801517558a5f4b68e909e0602d451fa72d
Igor Abdrakhimov <igorabd@amazon.com>
2025-04-28 13:05:57 -0700

    Fix race condition in service tests

--- jobs/tests/v2ClientTests.cpp
+++ jobs/tests/v2ClientTests.cpp
@@ -65,8 +65,8 @@ static std::shared_ptr<Aws::Crt::Mqtt5::Mqtt5Client> s_createProtocolClient5(Aws
                 {
                     std::lock_guard<std::mutex> guard(lock);
                     connected = true;
+                    signal.notify_all();
                 }
-                signal.notify_all();
             });
 
         client = Aws::Crt::Mqtt5::Mqtt5Client::NewMqtt5Client(mqtt5Options, allocator);
@@ -131,8 +131,8 @@ static std::shared_ptr<Aws::Crt::Mqtt::MqttConnection> s_createProtocolClient311
             {
                 std::lock_guard<std::mutex> guard(lock);
                 connected = true;
+                signal.notify_all();
             }
-            signal.notify_all();
         };
 
         auto uuid = Aws::Crt::UUID().ToString();
