06ed104ef1851cb54d8d36e8e1df19eccc8dfb52
Steve Kim <sbstevek@amazon.com>
2025-08-14 09:27:26 -0700

    remove samples used as tests from codebuild job

--- codebuild/samples/connect-linux.sh
+++ codebuild/samples/connect-linux.sh
@@ -4,7 +4,7 @@ set -e
 
 env
 
-pushd $CODEBUILD_SRC_DIR/samples/mqtt/basic_connect
+pushd $CODEBUILD_SRC_DIR/samples/mqtt5/mqtt5_pubsub
 
 mkdir _build
 cd _build
@@ -14,18 +14,18 @@ make -j
 ENDPOINT=$(aws secretsmanager get-secret-value --secret-id "ci/endpoint" --query "SecretString" | cut -f2 -d":" | sed -e 's/[\\\"\}]//g')
 
 echo "Basic Connect test"
-./basic-connect --endpoint $ENDPOINT --key /tmp/privatekey.pem --cert /tmp/certificate.pem
+./mqtt5_pubsub --endpoint $ENDPOINT --key /tmp/privatekey.pem --cert /tmp/certificate.pem
 
 popd
+# websocket test may need to be readded using mqtt5 client sample
+# pushd $CODEBUILD_SRC_DIR/samples/mqtt/websocket_connect
 
-pushd $CODEBUILD_SRC_DIR/samples/mqtt/websocket_connect
+# mkdir _build
+# cd _build
+# cmake -DCMAKE_PREFIX_PATH=/tmp/install ..
+# make -j
 
-mkdir _build
-cd _build
-cmake -DCMAKE_PREFIX_PATH=/tmp/install ..
-make -j
+# echo "Websocket test"
+# ./websocket-connect --endpoint $ENDPOINT --signing_region us-east-1
 
-echo "Websocket test"
-./websocket-connect --endpoint $ENDPOINT --signing_region us-east-1
-
-popd
+# popd
